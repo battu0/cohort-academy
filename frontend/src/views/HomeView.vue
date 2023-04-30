d121a14d57bd2bac605dcbc3d4a7c3c7d3677bff
Batu Ozturk <batuozturk@protonmail.com>
2023-04-30 10:18:48 +0300

    add feed area and user post components and home view

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import UserPost from '../components/UserPost.vue'
+</script>
+
+<template>
+  <main class="posts-container">
+    <UserPost />
+    <UserPost />
+    <UserPost />
+    <UserPost />
+    <UserPost />
+  </main>
+</template>
+
+<style scoped>
+.posts-container {
+  display: flex;
+  flex-direction: column-reverse;
+}
+</style>
