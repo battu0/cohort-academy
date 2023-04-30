aa1ded58a88076b60c11db74d9fcfb095aacc36a
Batu Ozturk <batuozturk@protonmail.com>
2023-04-30 10:24:42 +0300

    change style: single double braces to single

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,13 +1,13 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
-import App from "./App.vue";
-import router from "./router";
+import { createApp } from "vue"
+import { createPinia } from "pinia"
+import App from "./App.vue"
+import router from "./router"
 
-import "./assets/main.css";
+import "./assets/main.css"
 
-const app = createApp(App);
-const pinia = createPinia();
-app.use(pinia);
-app.use(router);
+const app = createApp(App)
+const pinia = createPinia()
+app.use(pinia)
+app.use(router)
 
-app.mount("#app");
+app.mount("#app")
