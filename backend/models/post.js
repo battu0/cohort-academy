f6be30efb2edce6a6c07ac2a721eee81e9c382f0
Batu Ozturk <batuozturk@protonmail.com>
2023-05-06 15:07:58 +0300

    refactor post model and import JSON data to posts collection

--- backend/models/post.js
+++ backend/models/post.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose")
 const postSchema = new mongoose.Schema({
   name: String,
+  avatar: String,
   username: String,
   publishTime: String,
+  postTitle: String,
   content: String,
 })
 
