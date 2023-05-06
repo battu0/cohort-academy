bce035507431e6cfafabbaf7f911acb4eb3b5ec3
Batu Ozturk <batuozturk@protonmail.com>
2023-05-06 15:33:30 +0300

    define prop called post

--- frontend/src/components/UserPost.vue
+++ frontend/src/components/UserPost.vue
@@ -1,34 +1,25 @@
+<script setup>
+defineProps({
+  post: {
+    type: Object,
+    required: true,
+  },
+})
+</script>
 <template>
-  <div class="posts">
+  <div class="post">
     <div class="post">
       <div class="post__header">
-        <div class="post__author-logo">{{ post.logo }}</div>
+        <div class="post__author-logo">{{ post.avatar }}</div>
         <div class="post__author-name">{{ post.name }}</div>
-        <div class="post__author-slug">{{ post.slug }}</div>
+        <div class="post__author-slug">{{ post.username }}</div>
         <div class="post__publish-time">{{ post.publish_time }}</div>
       </div>
       <div class="post__main">
-        <h3 class="post__title">{{ post.title }}</h3>
+        <h3 class="post__title">{{ post.post_title }}</h3>
         <div class="post__content">{{ post.content }}</div>
       </div>
     </div>
-    <!-- <div class="post">
-      <div class="post__header">
-        <div class="post__author-logo">Logo</div>
-        <div class="post__author-name">Ben Mishali</div>
-        <div class="post__author-slug">@benmishali</div>
-        <div class="post__publish-time">14min</div>
-      </div>
-      <div class="post__main">
-        <h3 class="post__title">Post Title</h3>
-        <div class="post__content">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum sit
-          voluptatem dolores adipisci expedita officia voluptatibus fuga est
-          ullam et. Laboriosam praesentium ad nulla dolores quibusdam
-          dignissimos necessitatibus temporibus ipsa.
-        </div>
-      </div>
-    </div> -->
   </div>
 </template>
 <style>
